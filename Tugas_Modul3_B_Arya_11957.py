a0712eb51ef4836da075f8904b008ad58335cd6a
Arya Nugraha <220711957@students.uajy.ac.id>
2024-10-09 15:24:20 +0700

    Update Tugas_Modul3_B_Arya_11957.py

--- Tugas_Modul3_B_Arya_11957.py
+++ Tugas_Modul3_B_Arya_11957.py
@@ -79,8 +79,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #00736e;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'D:\Funiversity\Sem5\ML\Tugas3_B_11957'
-    model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = r'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
